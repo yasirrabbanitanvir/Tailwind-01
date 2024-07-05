5540bfdc635377e4ade804100f7f585b1d7d850f
Yasir Rabbani Tanvir <yasirrabbanitanvir@gmail.com>
2024-07-05 12:22:10 +0600

    updates

--- my-project/src/MainContent.jsx
+++ my-project/src/MainContent.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const MainContent = () => {
   return (
-    <div className="p-6 md:p-8 max-w-lg mx-auto bg-white rounded-xl shadow-lg space-y-6 dark:bg-gray-800 dark:text-gray-300">
+    <div className="p-6 md:p-8 max-w-lg mx-auto bg-white rounded-xl shadow-lg space-y-6 dark:bg-gray-800 dark:text-gray-300 h-screen">
       <h1 className="text-3xl md:text-4xl font-bold text-center text-gray-900 dark:text-white">
         Welcome to the Dark Mode Toggle App
       </h1>
       <p className="text-lg md:text-xl text-gray-700 dark:text-gray-400 leading-relaxed">
-      It enables users to easily switch between light and dark display modes on their devices, such as smartphones, tablets or laptops.
+     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi odio nobis veniam cumque quod, eaque nesciunt minus iure esse nam excepturi unde? Laborum harum amet nostrum officiis voluptatem ipsa fugiat dignissimos voluptate id rem architecto ratione rerum, dolor, explicabo nisi molestiae obcaecati nihil impedit ad consequatur vero temporibus perspiciatis voluptatibus excepturi. Ab veniam nobis nemo perferendis neque fugiat iusto facilis blanditiis illo a recusandae id magnam voluptates, laudantium temporibus hic voluptas omnis cumque placeat vitae. Ea fugit ab minus exercitationem!
       </p>
     </div>
   );
